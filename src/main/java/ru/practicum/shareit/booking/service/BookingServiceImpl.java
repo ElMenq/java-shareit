a7280aed0346fdf2c3bbeb17d70f8bae6c3f3794
Vyacheslav <Delada12@yandex.ru>
2024-06-12 22:09:18 +0300

    добавления сохранения в бузу данных

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.booking.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,11 +40,12 @@ public class BookingServiceImpl implements BookingService {
     private final ItemService itemService;
     private final UserService userService;
 
-    private BookingMapper bookingMapper;
 
-    private ItemMapper itemMapper;
+    private final BookingMapper bookingMapper;
 
-    private UserMapper userMapper;
+    private final ItemMapper itemMapper;
+
+    private final UserMapper userMapper;
 
     @Override
     @Transactional
