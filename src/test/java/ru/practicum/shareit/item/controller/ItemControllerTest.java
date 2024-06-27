7902040051379467804bdbca850972fcb0d814aa
Vyacheslav <Delada12@yandex.ru>
2024-06-27 15:49:19 +0300

    добавление тестов

--- src/test/java/ru/practicum/shareit/item/controller/ItemControllerTest.java
+++ src/test/java/ru/practicum/shareit/item/controller/ItemControllerTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -38,7 +37,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
-@SpringBootTest
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ItemControllerTest {
 
